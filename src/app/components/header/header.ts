9ea67bc62e2f2384c55df6dd39c4e48dad96e9c9
JhonnyDiaz4753 <jhonnydiaz@estudiante.uniajc.edu.co>
2025-10-11 23:11:23 -0500

    se soluciono el problemas variados principalmente delas imagenes

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
+  isMenuOpen = false;
 
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen = false;
+  }
 }
