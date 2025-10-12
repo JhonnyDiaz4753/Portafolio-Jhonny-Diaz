f89cab940c31442b2c2204c82cc40e7a5db8ed9c
JhonnyDiaz4753 <jhonnydiaz@estudiante.uniajc.edu.co>
2025-10-11 19:47:47 -0500

    first commit

---
+++ src/app/app.ts
@@ -0,0 +1,19 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Hero } from './components/hero/hero';
+import { About } from './components/about/about';
+import { Projects } from './components/projects/projects';
+import { Contact } from './components/contact/contact';
+import { Footer } from './components/footer/footer';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Header, Hero, About, Projects, Contact, Footer],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('portafolio');
+}
