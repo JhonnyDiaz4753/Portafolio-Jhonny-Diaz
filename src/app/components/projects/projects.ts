0c35ba2aa4b35004b495e4924de775cf2cc3f944
JhonnyDiaz4753 <jhonnydiaz@estudiante.uniajc.edu.co>
2026-08-28 14:46:49 -0500

    feat: actualiza tarjetas de proyectos del portafolio

--- src/app/components/projects/projects.ts
+++ src/app/components/projects/projects.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { Project } from '../../models/Proyect';
 import { NgOptimizedImage } from '@angular/common';
-import { TiltDirective } from '../../directives/Tilt_directive';
 import { RevealDirective } from '../../directives/Reveal_directive';
 @Component({
   selector: 'app-projects',
-  imports: [NgOptimizedImage, TiltDirective, RevealDirective],
+  imports: [NgOptimizedImage, RevealDirective],
   templateUrl: './projects.html',
   styleUrl: './projects.css'
 })
